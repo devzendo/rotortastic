4028ebcc39ce48386c363c988bd77df173aa4e40
Matt Gumbley <matt.gumbley@devzendo.org>
2020-10-16 17:58:36 +0100

    Add script to run lint, and fix warnings.

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 
 const App: React.FunctionComponent = () => {
-    return <h1>rotortastic</h1>;
+  return <h1>rotortastic</h1>;
 };
 
-ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
+ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
