c333fc2df3c1e530e6d159c9fd662d4c2b53bab3
Matt Gumbley <matt.gumbley@devzendo.org>
2020-11-13 16:19:20 +0000

    Add s3-upload configuration

---
+++ aws-upload.conf.js
@@ -0,0 +1,10 @@
+module.exports = {
+    credentials:"aws-credentials.json",
+    bucketName:"rotortastic-devzendo-org",
+    patterns:[
+        "*.js",
+        //"stylesheets/default.css",
+        //"images/**/*.jpg",
+        "index.html"
+    ]
+}
