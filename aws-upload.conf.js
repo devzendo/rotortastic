ee5956e434d12bd8f2aea7b5b053efe8b507e2d2
Matt Gumbley <matt.gumbley@devzendo.org>
2020-11-13 17:13:18 +0000

    Install s3-upload globally; copy the s3-upload control files & credentials to the builder dist before upload.

--- aws-upload.conf.js
+++ aws-upload.conf.js
@@ -2,7 +2,10 @@ module.exports = {
     credentials:"aws-credentials.json",
     bucketName:"rotortastic-devzendo-org",
     patterns:[
-        "*.js",
+        // Note: MUST specify precisely which .js files are uploaded, DO NOT use wildcards.
+        // aws-upload-conf.js (this file) and aws-credentials.json will be copied to the builder's dist folder
+        // for upload. DO NOT UPLOAD THEM!
+        "bundle.js",
         //"stylesheets/default.css",
         //"images/**/*.jpg",
         "index.html"
