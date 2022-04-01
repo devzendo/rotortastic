262d137508d46e8b6d55fbe2ebdff0207c337fe7
Matt Gumbley <matt.gumbley@devzendo.org>
2022-04-01 16:48:35 +0100

    Modernise Prettier config; fix lint problems.

--- src/app.spec.tsx
+++ src/app.spec.tsx
@@ -1,15 +1,15 @@
-import React from "react"
+import React from 'react';
 
-import Enzyme, { mount } from "enzyme"
-import Adapter from "enzyme-adapter-react-16"
+import Enzyme, { mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-import { App } from "./app"
+import { App } from './app';
 
-Enzyme.configure({ adapter: new Adapter() })
+Enzyme.configure({ adapter: new Adapter() });
 
-describe("App", () => {
-  it("should have show the app name in a h1 header", () => {
-    const comp = mount(<App />)
-    expect(comp.containsMatchingElement(<h1>rotortastic</h1>)).toBeTruthy()
-  })
-})
+describe('App', () => {
+  it('should have show the app name in a h1 header', () => {
+    const comp = mount(<App />);
+    expect(comp.containsMatchingElement(<h1>rotortastic</h1>)).toBeTruthy();
+  });
+});
--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,5 @@
-import * as React from "react"
+import * as React from 'react';
 
 export const App: React.FunctionComponent = () => {
-  return <h1>rotortastic</h1>
-}
+  return <h1>rotortastic</h1>;
+};
